994f52a8ea78360da87ea36f672d7853a837b3de
Neil Davenport <neil@bynds.com>
2016-12-28 23:31:42 -0800

    Merge improvements from Alexander

--- uhttp_signature/authed_urequests.py
+++ uhttp_signature/authed_urequests.py
@@ -3,39 +3,18 @@ try:
 except ImportError:
     import requests
 try:
-    from utime import localtime as gmt_time
-except ImportError:
-    from time import localtime as gmt_time
-try:
     import uhmac as hmac
 except ImportError:
     import hmac
-
 try:
     from uhttp_signature import sign
 except ImportError:
     import httpsig.sign as sign
 
+
 class SignatureException(Exception):
     pass
 
-def httpdate(dt):
-    """Return a string representation of a date according to RFC 1123
-    (HTTP/1.1).
-
-    The supplied date must be in UTC.
-
-    """
-    try:
-        dt_year, dt_month, dt_day, dt_hour, dt_minute, dt_second, dt_weekday, dt_y = dt
-    except ValueError:
-        dt_year, dt_month, dt_day, dt_hour, dt_minute, dt_second, dt_weekday, dt_y, _ = dt
-    del(dt)
-    weekday = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][dt_weekday]
-    month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
-             "Oct", "Nov", "Dec"][dt_month - 1]
-    return "%s, %02d %s %04d %02d:%02d:%02d GMT" % (weekday, dt_day, month,
-        dt_year, dt_hour, dt_minute, dt_second)
 
 def make_request(url, key_id, secret, method='GET', debug=False, headers=None, **kwargs):
     try:
@@ -46,9 +25,9 @@ def make_request(url, key_id, secret, method='GET', debug=False, headers=None, *
         host_port = url.split('//')[1].split('/', 1)[0]
         path = ''
     path = '/' + path
-    
-    hs = sign.HeaderSigner(key_id=key_id, secret=secret, headers=['(request-target)','host','date',])
-    unsigned = { 'Host': host_port, 'Date': httpdate(gmt_time()),}
+
+    hs = sign.HeaderSigner(key_id=key_id, secret=secret, headers=['(request-target)', 'host', 'date'])
+    unsigned = { 'Host': host_port, 'Date': time.strftime('%a, %d %b %Y %H:%M:%S GMT')}
     lower_signed = hs.sign(unsigned, method=method, path=path)
     signed = {}
     signed.update({k[0:1:].upper()+k[1::]: v for k, v in lower_signed.items()})
@@ -62,16 +41,13 @@ def make_request(url, key_id, secret, method='GET', debug=False, headers=None, *
     except TypeError:
         return getattr(requests, method.lower())(url, headers=signed, **kwargs)
 
-def validate_response(response, secret):
-    try:
-        return hmac.compare_digest(response.content_hmac.encode('ascii'), hmac.new(secret, msg=b'date: '+response.date+response.text).hexdigest())
-    except AttributeError:
-        return hmac.compare_digest(response.headers['Content-HMAC'][1:-1:].encode('ascii'), hmac.new(secret, msg=b'date: '+response.headers['Date'].encode('ascii')+response.text.encode('ascii'), digestmod='sha256').hexdigest().encode('ascii'))
-    
+
 def make_validated_request(url, key_id, secret, method='GET', debug=False, headers=None, **kwargs):
-    response = make_request(url, key_id, secret, method='GET', debug=False, headers=None, **kwargs)
-    if validate_response(response, secret):
-        return response
-    else:
-        raise SignatureException
-     
+    response = make_request(url, key_id, secret, method=method, debug=debug, headers=headers, **kwargs)
+    if not hasattr(response, 'content_hmac'):
+        if response.content == b'{"detail":"Invalid signature."}':
+            raise SignatureException('Request is rejected: Invalid signature')
+        raise SignatureException('Response is not signed')
+    if not hmac.compare_digest(response.content_hmac.encode('ascii'), hmac.new(secret, msg=b'date: '+response.date+response.text).hexdigest()):
+        raise SignatureException('Invalid response signature')
+    return response
