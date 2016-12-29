2f30a7d781408ffa2175bae52b956bb228557681
Neil Davenport <neil@bynds.com>
2016-12-28 23:10:34 -0800

    Adding make_validated_request

--- uhttp_signature/authed_urequests.py
+++ uhttp_signature/authed_urequests.py
@@ -12,9 +12,12 @@ except ImportError:
     import hmac
 
 try:
-    import uhttp_signature.sign as sign
+    from uhttp_signature import sign
 except ImportError:
     import httpsig.sign as sign
+
+class SignatureException(Exception):
+    pass
 
 def httpdate(dt):
     """Return a string representation of a date according to RFC 1123
@@ -34,7 +37,7 @@ def httpdate(dt):
     return "%s, %02d %s %04d %02d:%02d:%02d GMT" % (weekday, dt_day, month,
         dt_year, dt_hour, dt_minute, dt_second)
 
-def make_request(url, key_id, secret, method='GET', debug=False):
+def make_request(url, key_id, secret, method='GET', debug=False, headers=None, **kwargs):
     try:
         # http://server.name:8000/path/and/more?attributes=True
         host_port, path = url.split('//')[1].split('/', 1)
@@ -51,13 +54,24 @@ def make_request(url, key_id, secret, method='GET', debug=False):
     signed.update({k[0:1:].upper()+k[1::]: v for k, v in lower_signed.items()})
     del lower_signed
 
+    if headers:
+        signed.update(headers)
+
     try:
-        return getattr(requests, method.lower())(url, headers=signed, debug=debug)
+        return getattr(requests, method.lower())(url, headers=signed, debug=debug, **kwargs)
     except TypeError:
-        return getattr(requests, method.lower())(url, headers=signed)
+        return getattr(requests, method.lower())(url, headers=signed, **kwargs)
 
 def validate_response(response, secret):
     try:
-        return hmac.compare_digest(response.content_hmac.encode('utf-8'), hmac.new(secret, msg=b'date: '+response.date+response.text).hexdigest())
+        return hmac.compare_digest(response.content_hmac.encode('ascii'), hmac.new(secret, msg=b'date: '+response.date+response.text).hexdigest())
     except AttributeError:
-        return hmac.compare_digest(response.headers['Content-HMAC'][1:-1:].encode('utf-8'), hmac.new(secret, msg=b'date: '+response.headers['Date'].encode('utf-8')+response.text.encode('utf-8'), digestmod='sha256').hexdigest().encode('utf-8'))
+        return hmac.compare_digest(response.headers['Content-HMAC'][1:-1:].encode('ascii'), hmac.new(secret, msg=b'date: '+response.headers['Date'].encode('ascii')+response.text.encode('ascii'), digestmod='sha256').hexdigest().encode('ascii'))
+    
+def make_validated_request(url, key_id, secret, method='GET', debug=False, headers=None, **kwargs):
+    response = make_request(url, key_id, secret, method='GET', debug=False, headers=None, **kwargs)
+    if validate_response(response, secret):
+        return response
+    else:
+        raise SignatureException
+     
