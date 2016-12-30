20191caa248bf7b0ddc03a5701c79ca73bd6304a
Neil Davenport <neil@bynds.com>
2016-12-29 21:45:35 -0800

    Adding back httpdate due to absence of strftime on some platforms

--- uhttp_signature/authed_urequests.py
+++ uhttp_signature/authed_urequests.py
@@ -10,10 +10,33 @@ try:
     from uhttp_signature import sign
 except ImportError:
     import httpsig.sign as sign
+try:
+    import utime as time
+    from utime import localtime as gmt_time
+except ImportError:
+    import time
+    from time import localtime as gmt_time
 
 
 class SignatureException(Exception):
     pass
+
+
+def httpdate(dt):
+    """Return a string representation of a date according to RFC 1123
+    (HTTP/1.1).
+    The supplied date must be in UTC.
+    """
+    try:
+        dt_year, dt_month, dt_day, dt_hour, dt_minute, dt_second, dt_weekday, dt_y = dt
+    except ValueError:
+        dt_year, dt_month, dt_day, dt_hour, dt_minute, dt_second, dt_weekday, dt_y, _ = dt
+    del(dt)
+    weekday = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][dt_weekday]
+    month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+             "Oct", "Nov", "Dec"][dt_month - 1]
+    return "%s, %02d %s %04d %02d:%02d:%02d GMT" % (weekday, dt_day, month,
+        dt_year, dt_hour, dt_minute, dt_second)
 
 
 def make_request(url, key_id, secret, method='GET', debug=False, headers=None, **kwargs):
@@ -27,7 +50,10 @@ def make_request(url, key_id, secret, method='GET', debug=False, headers=None, *
     path = '/' + path
 
     hs = sign.HeaderSigner(key_id=key_id, secret=secret, headers=['(request-target)', 'host', 'date'])
-    unsigned = { 'Host': host_port, 'Date': time.strftime('%a, %d %b %Y %H:%M:%S GMT')}
+    try:
+        unsigned = { 'Host': host_port, 'Date': time.strftime('%a, %d %b %Y %H:%M:%S GMT')}
+    except AttributeError:
+        unsigned = { 'Host': host_port, 'Date': httpdate(gmt_time()),}
     lower_signed = hs.sign(unsigned, method=method, path=path)
     signed = {}
     signed.update({k[0:1:].upper()+k[1::]: v for k, v in lower_signed.items()})
